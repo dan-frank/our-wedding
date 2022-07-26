ecbb52440784d4a55fd6eb0cae9a5f75b21b56c6
Daniel Lucas <dan@Daniels-MacBook-Pro.local>
2022-07-26 16:44:34 +0100

    Added git-lfs for video types and included images

---
+++ src/components/banner/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./banner.css";
+
+function Banner() {
+  return (
+    <div class="banner-container">
+      <div class="banner-circle-container">
+        <div class="banner-circle">
+          <div
+            class="play-button popup-trigger"
+            data-target="popup-video"
+          ></div>
+
+          <div class="banner-image">
+            <div class="image-shape square">
+              <img src="./images/kiss-small.jpg" alt="bride and groom kiss" />
+            </div>
+          </div>
+
+          <div class="banner-content">
+            <h2 class="font-serif-2 font-light font-size-500 font-light m-y-sm">
+              Our Wedding
+            </h2>
+            <h1 class="font-serif-1 font-light font-size-600 font-light m-y-sm">
+              The Perfect Day
+            </h1>
+            <p class="font-sans-serif">
+              <a href="/" class="button-line-white">
+                <span>Join Us As We Remember Our Special Day</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
