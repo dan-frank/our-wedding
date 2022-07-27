987634862f315a0117cca96f8dbb3581978b610d
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 18:19:01 +0100

    Implemented Button component on Footer + Improved css

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./button.css";
 
-const Button = ({ text, url, modifiers }) => {
+const Button = ({ text, url, modifiers, blank }) => {
   if (url) {
+    const target = blank ? `target="_blank" rel="noreferrer"` : "";
+
     return (
-      <a href={url} className={modifiers}>
+      <a href={url} className={modifiers} {...target}>
         {text}
       </a>
     );
