adbea329959cdfaabfd21e72c91dea04af751283
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 21:49:31 +0100

    Completed gallery component

--- src/components/gallery/index.jsx
+++ src/components/gallery/index.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./gallery.css";
 
-const Gallery = () => {
-  return <div></div>;
+import Image from "../image";
+
+const Gallery = ({ size, images }) => {
+  return (
+    <div className="images">
+      {images.map((image) => {
+        image.size = size;
+        return <Image url={image.url} alt={image.alt} size={image.size} />;
+      })}
+    </div>
+  );
 };
 
 export default Gallery;
