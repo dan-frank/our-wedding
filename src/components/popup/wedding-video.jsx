79ae3399fd8e710f28e707f6e1ea9276d3e3d718
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 21:41:43 +0100

    Applied video to PopupWeddingVideo

--- src/components/popup/wedding-video.jsx
+++ src/components/popup/wedding-video.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import "./popup.css";
 
 import Popup from ".";
+import Video from "../image/video";
 
 const PopupWeddingVideo = ({ setShowPopup }) => {
   return (
     <Popup
       children={
-        <div class="image is-16by9">
-          <video id="weddingVideo" controls>
-            <source src="videos/wedding.mp4" type="video/mp4" />
-            <source src="videos/wedding.webm" type="video/webm" />
-            <source src="videos/wedding.m4v" type="video/m4v" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+        <Video
+          size="is-16by9"
+          videos={[
+            {
+              url: "videos/wedding.mp4",
+              type: "video/mp4",
+            },
+            {
+              url: "videos/wedding.webm",
+              type: "video/webm",
+            },
+            {
+              url: "videos/wedding.m4v",
+              type: "video/m4v",
+            },
+          ]}
+        />
       }
       setShowPopup={setShowPopup}
     />
