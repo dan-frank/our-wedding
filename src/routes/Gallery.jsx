2b1e0771ba7663c7e840e06c0daf5caa09433e00
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 22:05:24 +0100

    Added first gallery

--- src/routes/Gallery.jsx
+++ src/routes/Gallery.jsx
@@ -7,6 +7,7 @@ import ContentCard from "../components/content/content-card";
 import Nav from "../components/nav";
 import Spacer from "../components/spacer";
 import Video from "../components/image/video";
+import Gallery from "../components/gallery";
 
 const GalleryRoute = () => {
   return (
@@ -44,6 +45,56 @@ const GalleryRoute = () => {
                     ]}
                   />
                   <Spacer size={500} />
+                  <Gallery
+                    size="is-3by4"
+                    images={[
+                      {
+                        url: "images/gallery/1-pre-ceremony/100.jpeg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/200.jpeg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/300.jpeg",
+                        alt: "",
+                      },
+                      { url: "images/gallery/1-pre-ceremony/400.JPG", alt: "" },
+                      { url: "images/gallery/1-pre-ceremony/500.jpg", alt: "" },
+                      { url: "images/gallery/1-pre-ceremony/650.jpg", alt: "" },
+                      {
+                        url: "images/gallery/1-pre-ceremony/600.jpeg",
+                        alt: "",
+                      },
+                      { url: "images/gallery/1-pre-ceremony/700.JPG", alt: "" },
+                      {
+                        url: "images/gallery/1-pre-ceremony/800.jpeg",
+                        alt: "",
+                      },
+                      { url: "images/gallery/1-pre-ceremony/900.jpg", alt: "" },
+                      {
+                        url: "images/gallery/1-pre-ceremony/1000.jpg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/1100.jpg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/1200.jpg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/1300.jpg",
+                        alt: "",
+                      },
+                      {
+                        url: "images/gallery/1-pre-ceremony/1400.jpg",
+                        alt: "",
+                      },
+                    ]}
+                  />
                 </>
               }
             />
