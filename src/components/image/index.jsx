cdabde92ed71f1ba54fef4dcfe4d1f23d7564512
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 12:50:18 +0100

    Coverted image HTML to react component

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./image.css";
 
 const Image = ({ image, alt, size }) => {
-  return <div></div>;
+  return (
+    <div class={"image-shape " + size}>
+      <img src={image} alt={alt} />
+    </div>
+  );
 };
 
 export default Image;
