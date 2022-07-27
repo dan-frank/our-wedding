5642181924d77bbaf1d74f882952dda38de30a8e
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 13:23:37 +0100

    Coverted spcaer HTML to react component

--- src/components/spacer/index.jsx
+++ src/components/spacer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./spacer.css";
 
 const Spacer = ({ size }) => {
-  return <div></div>;
+  return <div class={"spacer-" + size}></div>;
 };
 
 export default Spacer;
