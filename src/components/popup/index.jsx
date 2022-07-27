1b9be98b7a8e17b679459e56a9c0d8a7f7f32668
dan-frank <dan.frank.lucas@gmail.com>
2022-07-27 18:34:52 +0100

    Converted HTML to React component

--- src/components/popup/index.jsx
+++ src/components/popup/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./popup.css";
 
-const Popup = () => {
-  return <div></div>;
+const Popup = ({ id, children }) => {
+  return (
+    <div id={"popup--" + id} className="popup">
+      <div className="popup-content">{children}</div>
+    </div>
+  );
 };
 
 export default Popup;
