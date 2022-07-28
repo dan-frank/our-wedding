642e268d1fe60ef3c24f3ca5754e5ec88f01c655
dan-frank <dan.frank.lucas@gmail.com>
2022-07-28 01:35:16 +0100

    Made Nav a 'global' component in App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "normalize.css";
 
 import HomeRoute from "./routes/Home";
 import GalleryRoute from "./routes/Gallery";
+import Nav from "./components/nav";
 
 function App() {
   return (
     <Router>
       <div class="main">
+        <Nav />
         <Routes>
           <Route exact path="/" element={<HomeRoute />}></Route>
           <Route exact path="/gallery" element={<GalleryRoute />}></Route>
