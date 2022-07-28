20f39509cbbea71d297fb3b5d6aa470bbdf9d87b
dan-frank <dan.frank.lucas@gmail.com>
2022-07-28 18:12:07 +0100

    Now scroll to top when changing route #33

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import "normalize.css";
 import HomeRoute from "./routes/Home";
 import GalleryRoute from "./routes/Gallery";
 import Nav from "./components/nav";
+import ScrollToTop from "./wrappers/scroll-to-top";
 
 function App() {
   return (
     <Router>
       <div class="main">
-        <Nav />
-        <Routes>
-          <Route exact path="/" element={<HomeRoute />}></Route>
-          <Route exact path="gallery" element={<GalleryRoute />}></Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <ScrollToTop>
+          <Nav />
+          <Routes>
+            <Route exact path="/" element={<HomeRoute />}></Route>
+            <Route exact path="gallery" element={<GalleryRoute />}></Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </ScrollToTop>
       </div>
     </Router>
   );
