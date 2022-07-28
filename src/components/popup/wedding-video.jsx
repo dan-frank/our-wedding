77934f79760399577c9d7261e1cbfb0e948483b1
dan-frank <dan.frank.lucas@gmail.com>
2022-07-28 12:29:29 +0100

    Added wedding iframe to popup

--- src/components/popup/wedding-video.jsx
+++ src/components/popup/wedding-video.jsx
@@ -1,29 +1,16 @@
 import React from "react";
 import "./popup.css";
 
+import IFrame from "../image/iframe";
 import Popup from ".";
-import Video from "../image/video";
 
 const PopupWeddingVideo = ({ setShowPopup }) => {
   return (
     <Popup
       children={
-        <Video
+        <IFrame
+          url="https://player.vimeo.com/video/734325722?h=2ce8b63b18&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
           size="is-16by9"
-          videos={[
-            {
-              url: "videos/wedding.mp4",
-              type: "video/mp4",
-            },
-            {
-              url: "videos/wedding.webm",
-              type: "video/webm",
-            },
-            {
-              url: "videos/wedding.m4v",
-              type: "video/m4v",
-            },
-          ]}
         />
       }
       setShowPopup={setShowPopup}
