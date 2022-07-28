96b70f0b0f10bf3a4c3052ea5b5fb83c167a705c
dan-frank <dan.frank.lucas@gmail.com>
2022-07-28 01:56:10 +0100

    Removed old js/css init files

--- main.js
+++
@@ -1,9 +0,0 @@
-// @codekit-prepend "javascript/imports/jquery.js";
-// @codekit-prepend "javascript/imports/lightbox.js";
-// @codekit-prepend "javascript/imports/lazyload.js";
-// @codekit-prepend "javascript/imports/smoothscroll.js";
-
-// @codekit-append "javascript/parts/popup.js";
-// @codekit-append "javascript/parts/lazyload_trigger.js";
-
-var scroll = new SmoothScroll('a[href*="#"]');
